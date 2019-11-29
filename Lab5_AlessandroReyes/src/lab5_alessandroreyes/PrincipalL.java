1e1fd8f0319c00d7c86a499f43adcbe6a75d62bd
agreyest <56709605+agreyest@users.noreply.github.com>
2019-11-28 19:38:20 -0600

    Agregar a arbol
    terminada

--- Lab5_AlessandroReyes/src/lab5_alessandroreyes/PrincipalL.java
+++ Lab5_AlessandroReyes/src/lab5_alessandroreyes/PrincipalL.java
@@ -5,6 +5,8 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.DefaultListModel;
 import javax.swing.JOptionPane;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 
 public class PrincipalL extends javax.swing.JFrame {
 
@@ -44,7 +46,17 @@ public class PrincipalL extends javax.swing.JFrame {
         carreras.add(c);
         c = new Carrera("Terapia Fisica y Ocupacional", "Ciencias de la Salud", 9000, "Marlon");
         carreras.add(c);
+        //Estudiantes-----------------------------------------------------------------------------------------------
+        Estudiante es = new Estudiante("Alessandro", "Reyes", 11911224, 19, true, carreras.get(5));
+        estudiantes.add(es);
+        es = new Estudiante("Alvaro", "Reyes", 11611224, 24, true, carreras.get(5));
+        estudiantes.add(es);
+        es = new Estudiante("Daniel", "Trimarchi", 11711224, 20, true, carreras.get(5));
+        estudiantes.add(es);
+        es = new Estudiante("Adriana", "Parodi", 11811224, 19, true, carreras.get(10));
+        estudiantes.add(es);
         
+        //------------------------------------------------------------------------------------------------
         SpinnerNumberModel YY = new SpinnerNumberModel();
         YY.setMinimum(16);
         YY.setValue(16);
@@ -79,12 +91,14 @@ public class PrincipalL extends javax.swing.JFrame {
         bg_genero = new javax.swing.ButtonGroup();
         jd_listar_estudiantes = new javax.swing.JDialog();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jList1 = new javax.swing.JList<>();
+        jl_listar_estudiantes = new javax.swing.JList<>();
         btn_listarE_regresar = new javax.swing.JButton();
         jd_arbol = new javax.swing.JDialog();
         btn_arbol_regresar = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTree1 = new javax.swing.JTree();
+        jt_estudiantes = new javax.swing.JTree();
+        pop_op_estu = new javax.swing.JPopupMenu();
+        Agregar_arbol = new javax.swing.JMenuItem();
         jMenuBar1 = new javax.swing.JMenuBar();
         Acciones = new javax.swing.JMenu();
         jMenu3 = new javax.swing.JMenu();
@@ -209,10 +223,20 @@ public class PrincipalL extends javax.swing.JFrame {
                         .addGap(28, 28, 28))))
         );
 
-        jList1.setModel(new DefaultListModel());
-        jScrollPane1.setViewportView(jList1);
+        jl_listar_estudiantes.setModel(new DefaultListModel());
+        jl_listar_estudiantes.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jl_listar_estudiantesMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(jl_listar_estudiantes);
 
         btn_listarE_regresar.setText("Regresar");
+        btn_listarE_regresar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btn_listarE_regresarMouseClicked(evt);
+            }
+        });
 
         javax.swing.GroupLayout jd_listar_estudiantesLayout = new javax.swing.GroupLayout(jd_listar_estudiantes.getContentPane());
         jd_listar_estudiantes.getContentPane().setLayout(jd_listar_estudiantesLayout);
@@ -238,10 +262,15 @@ public class PrincipalL extends javax.swing.JFrame {
         );
 
         btn_arbol_regresar.setText("Regresar");
+        btn_arbol_regresar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btn_arbol_regresarMouseClicked(evt);
+            }
+        });
 
         javax.swing.tree.DefaultMutableTreeNode treeNode1 = new javax.swing.tree.DefaultMutableTreeNode("Carreras");
-        jTree1.setModel(new javax.swing.tree.DefaultTreeModel(treeNode1));
-        jScrollPane2.setViewportView(jTree1);
+        jt_estudiantes.setModel(new javax.swing.tree.DefaultTreeModel(treeNode1));
+        jScrollPane2.setViewportView(jt_estudiantes);
 
         javax.swing.GroupLayout jd_arbolLayout = new javax.swing.GroupLayout(jd_arbol.getContentPane());
         jd_arbol.getContentPane().setLayout(jd_arbolLayout);
@@ -267,6 +296,14 @@ public class PrincipalL extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
+        Agregar_arbol.setText("Agregar al Arbol");
+        Agregar_arbol.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Agregar_arbolActionPerformed(evt);
+            }
+        });
+        pop_op_estu.add(Agregar_arbol);
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         Acciones.setText("Acciones");
@@ -282,6 +319,11 @@ public class PrincipalL extends javax.swing.JFrame {
         jMenu3.add(jmi_crear_estudiantes);
 
         jmi_listar_estudiantes.setText("Listar");
+        jmi_listar_estudiantes.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_listar_estudiantesActionPerformed(evt);
+            }
+        });
         jMenu3.add(jmi_listar_estudiantes);
 
         Acciones.add(jMenu3);
@@ -317,6 +359,11 @@ public class PrincipalL extends javax.swing.JFrame {
         Acciones.add(jMenu4);
 
         jmi_arbol.setText("Arbol");
+        jmi_arbol.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_arbolActionPerformed(evt);
+            }
+        });
         Acciones.add(jmi_arbol);
 
         jMenuBar1.add(Acciones);
@@ -413,7 +460,118 @@ public class PrincipalL extends javax.swing.JFrame {
         jd_crear_estudiante.setModal(false);
         jd_crear_estudiante.setVisible(false);
         this.setVisible(true);
+        this.setLocationRelativeTo(this);
     }//GEN-LAST:event_btn_crearE_regresarMouseClicked
+
+    private void jmi_listar_estudiantesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_listar_estudiantesActionPerformed
+        DefaultListModel modeloLISTA = (DefaultListModel) jl_listar_estudiantes.getModel();
+        
+        for (int i = 0; i < estudiantes.size(); i++) {
+            modeloLISTA.addElement(new Estudiante(estudiantes.get(i).getNombre(), estudiantes.get(i).getApellido(), 
+            estudiantes.get(i).getCuenta(), estudiantes.get(i).getEdad(), estudiantes.get(i).isGenero(), estudiantes.get(i).getCarrera()));
+        }
+        jl_listar_estudiantes.setModel(modeloLISTA);
+        this.setVisible(false);
+        jd_listar_estudiantes.setModal(true);
+        jd_listar_estudiantes.pack();
+        jd_listar_estudiantes.setLocationRelativeTo(this);
+        jd_listar_estudiantes.setVisible(true);
+    }//GEN-LAST:event_jmi_listar_estudiantesActionPerformed
+
+    private void btn_listarE_regresarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_listarE_regresarMouseClicked
+        jd_listar_estudiantes.setModal(false);
+        jd_listar_estudiantes.setVisible(false);
+        
+        this.setVisible(true);
+        this.setLocationRelativeTo(this);
+    }//GEN-LAST:event_btn_listarE_regresarMouseClicked
+
+    private void jl_listar_estudiantesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jl_listar_estudiantesMouseClicked
+        if (jl_listar_estudiantes.getSelectedIndex() >= 0) {
+            if (evt.isMetaDown()) {
+                pop_op_estu.show(evt.getComponent(), evt.getX(), evt.getY());
+            }
+        }
+    }//GEN-LAST:event_jl_listar_estudiantesMouseClicked
+
+    private void Agregar_arbolActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Agregar_arbolActionPerformed
+        if (jl_listar_estudiantes.getSelectedIndex() >= 0) {
+            DefaultTreeModel modeloARBOL = (DefaultTreeModel) jt_estudiantes.getModel();
+            DefaultMutableTreeNode raiz
+                    = (DefaultMutableTreeNode) modeloARBOL.getRoot();
+            //obtener la persona a guardar
+            DefaultListModel modeloLISTA
+                    = (DefaultListModel) jl_listar_estudiantes.getModel();
+            String nombre, apellido;
+            int cuenta, edad;
+            boolean gene;
+            Carrera ca;
+            nombre = ((Estudiante) modeloLISTA.get(
+                    jl_listar_estudiantes.getSelectedIndex())).getNombre();
+            apellido = ((Estudiante) modeloLISTA.get(
+                    jl_listar_estudiantes.getSelectedIndex())).getApellido();
+            cuenta = ((Estudiante) modeloLISTA.get(
+                    jl_listar_estudiantes.getSelectedIndex())).getEdad();
+            edad = ((Estudiante) modeloLISTA.get(
+                    jl_listar_estudiantes.getSelectedIndex())).getCuenta();
+            gene = ((Estudiante) modeloLISTA.get(
+                    jl_listar_estudiantes.getSelectedIndex())).isGenero();
+            ca = ((Estudiante) modeloLISTA.get(
+                    jl_listar_estudiantes.getSelectedIndex())).getCarrera();
+            int centinela = -1;
+            boolean a = false;
+            for (int i = 0; i < raiz.getChildCount(); i++) {
+                if (raiz.getChildAt(i).toString().equals(ca)) {
+                    for (int j = 0; j < raiz.getChildAt(i).getChildCount(); j++) {
+                        if (raiz.getChildAt(i).getChildAt(j).toString().equals(nombre)) {
+                            a = true;
+                            JOptionPane.showMessageDialog(this, "la persona ya existe");
+                            break;
+                        } else if (j == raiz.getChildAt(i).getChildCount()-1
+                                && !raiz.getChildAt(i).getChildAt(j).toString().equals(nombre)) {
+                            a = true;
+                            DefaultMutableTreeNode p
+                                    = new DefaultMutableTreeNode(
+                                            new Estudiante(nombre, apellido, cuenta, edad, gene, ca));
+                            ((DefaultMutableTreeNode) raiz.getChildAt(i)).add(p);
+                            break;
+                        }//fin del elseif
+                    }//fin del for2
+                    if (a == true) {
+                        centinela = 1;
+                    }
+                }//fin if
+            }//fin for1
+            if (centinela == -1) {
+                DefaultMutableTreeNode n
+                        = new DefaultMutableTreeNode(ca);
+                DefaultMutableTreeNode p
+                        = new DefaultMutableTreeNode(
+                                new Estudiante(nombre, apellido, cuenta, edad, gene, ca));
+                n.add(p);
+                raiz.add(n);
+            }//fin if
+            modeloARBOL.reload();
+        } else {
+            JOptionPane.showMessageDialog(this, "No hay persona seleccionada");
+        }
+    }//GEN-LAST:event_Agregar_arbolActionPerformed
+
+    private void jmi_arbolActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_arbolActionPerformed
+        this.setVisible(false);
+        jd_arbol.setModal(true);
+        jd_arbol.pack();
+        jd_arbol.setLocationRelativeTo(this);
+        jd_arbol.setVisible(true);
+    }//GEN-LAST:event_jmi_arbolActionPerformed
+
+    private void btn_arbol_regresarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_arbol_regresarMouseClicked
+        jd_arbol.setModal(false);
+        jd_arbol.setVisible(false);
+        
+        this.setVisible(true);
+        this.setLocationRelativeTo(this);
+    }//GEN-LAST:event_btn_arbol_regresarMouseClicked
 
     /**
      * @param args the command line arguments
@@ -452,6 +610,7 @@ public class PrincipalL extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenu Acciones;
+    private javax.swing.JMenuItem Agregar_arbol;
     private javax.swing.ButtonGroup bg_genero;
     private javax.swing.JButton btn_arbol_regresar;
     private javax.swing.JButton btn_crearE_regresar;
@@ -464,7 +623,6 @@ public class PrincipalL extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JList<String> jList1;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
@@ -472,10 +630,10 @@ public class PrincipalL extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTree jTree1;
     private javax.swing.JDialog jd_arbol;
     private javax.swing.JDialog jd_crear_estudiante;
     private javax.swing.JDialog jd_listar_estudiantes;
+    private javax.swing.JList<String> jl_listar_estudiantes;
     private javax.swing.JMenuItem jmi_arbol;
     private javax.swing.JMenuItem jmi_crear_carreras;
     private javax.swing.JMenuItem jmi_crear_clases;
@@ -486,6 +644,8 @@ public class PrincipalL extends javax.swing.JFrame {
     private javax.swing.JMenuItem jmi_listar_estudiantes;
     private javax.swing.JMenuItem jmi_listar_maestros;
     private javax.swing.JSpinner js_estu_edad;
+    private javax.swing.JTree jt_estudiantes;
+    private javax.swing.JPopupMenu pop_op_estu;
     private javax.swing.JRadioButton rb_f;
     private javax.swing.JRadioButton rb_m;
     private javax.swing.JTextField tf_estudiante_apellido;
